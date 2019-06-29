062ba8b3fbcfd727cb26f73134ac313e86a7dc96
Kuldeepkp <37731301+Kuldeepkp@users.noreply.github.com>
2019-06-30 01:48:39 +0530

    Update MainActivity.java
    Forth Commit

--- app/src/main/java/com/kp/commonproject/MainActivity.java
+++ app/src/main/java/com/kp/commonproject/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Hello world", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Second Change", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Third Change", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Fourth Change", Toast.LENGTH_SHORT).show();
+
 
 }
 }
